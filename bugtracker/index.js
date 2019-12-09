4a00c45ae75f6485d75d83c0b86720005dee936c
DarkTechLC <cliciosa14@gmail.com>
2019-12-09 17:09:19 -0300

    Up

---
+++ bugtracker/index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+
+app.set('view engine', 'ejs')
+apt.set('views', path.resolve(__dirname, 'views'))
+
+app.get('/', (request, response) => {
+  response.render('home')
+})
+
+app.get('/soma', (request, response) => {
+  const a = parseInt(request.query.a)
+  const b = parseInt(request.query.b)
+  const soma = a + b
+  response.send('<h1>A soma é: ' + soma + '</h1>')
+})
+
+app.listen(3000)
